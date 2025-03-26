984d2be52116fb3e6f958c1bf05010c20f8bc16c
ibuildsoftware <ibuildsoftware@outlook.fr>
2025-03-26 22:13:20 +0100

    Complete sign up view and save model

--- todo/todo/views.py
+++ todo/todo/views.py
@@ -1,5 +1,16 @@
-from  django . shortcuts import render, redirect
+from  django . shortcuts import render, redirect # redirect from one page to another
+from django.contrib.auth.models import User # save user inputs into model
+from todo.models import TODOO # save user inputs into model
 
-
-def home(request):
+# sign up page
+def signup(request):
+    if request.method == 'POST': # get user inputs and save them in the model
+        fnm=request.POST.get('fnm')
+        emailid=request.POST.get('emailid')
+        pwd=request.POST.get('pwd')
+        print(fnm,emailid,pwd) # username, user email, password
+        my_user=User.objects.create_user(fnm,emailid,pwd)
+        my_user.save() # save model
+        return redirect('/loginn') # redirect user to login page
+    
     return render(request, 'signup.html')
